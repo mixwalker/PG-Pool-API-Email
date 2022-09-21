ff9fbff4087d3a450bbde2f10214d7f6bdc13d7d
mixwalker <44310036+mixwalker@users.noreply.github.com>
2022-09-21 19:47:06 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'mixwalkerz@gmail.com',
+        pass: 'eattqyqioaaikpso',
+    },
+});
+
+app.use(bodyParser.json())
+
+app.post('/mail/send', (req, res) => {
+    const to = req.body.to;
+    const subject = req.body.subject;
+    const text = req.body.text;
+
+    transporter.sendMail({
+        from: '"CDG" <whoami@gmail.com>',
+        to: to,
+        subject: subject,
+        text: text,
+        html: text,
+    }).then(() => {
+        res.status(200).json({
+            error: false,
+            msg: 'Sent successfully.'
+        })
+    }).catch(() => {
+        res.status(500).json({
+            error: true,
+            msg: 'Sent error.'
+        })
+    });
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
