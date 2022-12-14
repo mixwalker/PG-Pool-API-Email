974fecdd372d4c662692144ec91d7fbde70e339b
mixwalker <44310036+mixwalker@users.noreply.github.com>
2022-12-14 10:02:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     auth: {
-        user: 'mixwalkerz@gmail.com',
-        pass: 'eattqyqioaaikpso',
+        user: '',
+        pass: '',
     },
 });
 
